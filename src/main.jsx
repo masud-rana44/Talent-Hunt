1584c52f2d01f167d44ca98c77eb8629682785f0
masud-rana44 <mdmasudrana4488@gmail.com>
2023-11-24 03:30:21 +0600

    Implemented ui for login page

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import router from "./routes/Router";
+import AuthProvider from "./providers/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
